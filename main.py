3802aeea5d500214d01de367ae8ca7bd7569ea55
ashk123 <ashkqwe123ww@gmail.com>
2022-12-10 19:55:49 +0330

    add information about each search result, and fix some error

--- main.py
+++ main.py
@@ -4,9 +4,29 @@ import os
 import sys
 import urllib.parse
 
+# Config (I make a config file soon)
+inform_conf = True
+
 # define section
 VER = "1.9"
 OS = 0 if (sys.platform == "linux") else 0 if (sys.platform == "darwin") else 1 if (sys.platform == "win32") else 0
+HELP = """pikiTerm Help --
+--------------------------------
+[!] What is pikiTerm ?
+    pikiTerm is a wikipedia reader on terminal
+
+[+] Run
+    python3 main.py
+    OR
+    python3 main.py <content>
+
+[+] Version
+    python3 main.py -v
+    OR
+    python3 main.py --version
+
+2022 ashk123/Eiko
+-----------------------------"""
 
 more =  [
     'en',
@@ -27,14 +47,24 @@ def SaveArticle(Name, Value, sa) :
         ar_file.close()
     return True
 
-def CheckVersion(data) :
+def CheckArg(data) :
+    els = ["--", "==", '-', '=']
+
     if (data == "-v" or data == "--version") :
         print(f"pikiTerm 2022 V{VER}")
         sys.exit()
+    elif (data == "-h" or data == "--help") :
+        print(HELP)
+        sys.exit()
+    else :
+        for i in els :
+            if (i in data) :
+                print("Enter a valid content !")
+                sys.exit()
 
 Datas = {'SEARCH':'', 'TITLE':'', 'BODY':'', 'SELECTION':''}
 if (len(sys.argv) > 1) :
-    CheckVersion(sys.argv[1])
+    CheckArg(sys.argv[1])
     argv_user = ""
     for i in range(1, len(sys.argv)) :
         argv_user += sys.argv[i] + " "
@@ -49,13 +79,17 @@ else :
 if (query == False) :
     print("Enter valid content !")
     sys.exit()
+elif (query == "q") :
+    sys.exit()
 url_search = f"https://{cl}.wikipedia.org/w/index.php?search={query}&title=Special:Search&profile=advanced&fulltext=1&ns0=1&searchToken=7637hhwf6zvrreb4qxzqjusdb"
 
 res = requests.get(url_search)
 soup = BeautifulSoup(res.text, "html.parser")
 for i in range(5) :
     tag_span = soup.find("a", {'data-serp-pos':f'{str(i)}'})
-    Datas['SEARCH'] += str(i)+":"+tag_span.get_text() + ":"
+    date_inform = soup.find("div", "mw-search-result-data").get_text().split(",")[1];
+    date_form = " -" + date_inform
+    Datas['SEARCH'] += str(i)+":"+tag_span.get_text() + (date_form if (inform_conf == True) else "") + ":"
 
 i2 = Datas['SEARCH'].split(":")
 num = 0
@@ -71,6 +105,8 @@ except KeyboardInterrupt :
     print("\nyou cancel the proccess !")
     sys.exit()
 num = 0
+if (sel == "q") :
+    sys.exit()
 for i4 in Datas['SEARCH'] :
     i4 = Datas['SEARCH'].split(":")
     try :
